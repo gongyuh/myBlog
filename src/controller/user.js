34121fb3046b77bdc6c51d51e25c57afa08c0349
gongyuh <2736498007@qq.com>
2021-04-10 10:33:35 +0800

    md5-encryption

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,8 +1,12 @@
-const { exec,escape } = require('./../db/mysql') 
+const { exec,escape } = require('./../db/mysql')
+const {genPassword} = require('../utils/cryp')
 
 const loginCheck = (username,password) => {
     //防止sql注入
     username = escape(username)
+
+    //生成加密的密码
+    password = genPassword(password)
     password = escape(password)
 
     const sql = `
